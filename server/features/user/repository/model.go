7c2a6026cdc190a54e1ca4a91c622732595fc98e
dimasyudhana <dimas.yudhana@gmail.com>
2023-07-15 12:02:48 +0800

    refactor-member-and-message

--- server/features/user/repository/model.go
+++ server/features/user/repository/model.go
@@ -3,28 +3,37 @@ package repository
 import (
 	"time"
 
-	message "github.com/dimasyudhana/simple-chat/features/message/repository"
 	room "github.com/dimasyudhana/simple-chat/features/room/repository"
 	"github.com/dimasyudhana/simple-chat/features/user"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	UserID    string            `gorm:"primaryKey;type:varchar(45)"`
-	Username  string            `gorm:"type:varchar(225);not null"`
-	Email     string            `gorm:"type:varchar(225);not null;unique"`
-	Password  string            `gorm:"type:text;not null"`
-	CreatedAt time.Time         `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
-	UpdatedAt time.Time         `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
-	DeletedAt gorm.DeletedAt    `gorm:"index"`
-	Messages  []message.Message `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
-	Rooms     []room.Room       `gorm:"many2many:Members;foreignKey:UserID;joinForeignKey:UserID"`
+	UserID    string         `gorm:"primaryKey;type:varchar(45)"`
+	Username  string         `gorm:"type:varchar(225);not null"`
+	Email     string         `gorm:"type:varchar(225);not null;unique"`
+	Password  string         `gorm:"type:text;not null"`
+	CreatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Messages  []room.Room    `gorm:"many2many:Messages;foreignKey:UserID;joinForeignKey:UserID"`
+	Members   []room.Room    `gorm:"many2many:Members;foreignKey:UserID;joinForeignKey:UserID"`
 }
 
 type Members struct {
 	MemberID   string         `gorm:"primaryKey;type:varchar(45)"`
 	UserID     string         `gorm:"foreignKey;type:varchar(45)"`
 	RoomRoomID string         `gorm:"foreignKey;type:varchar(45)"`
+	CreatedAt  time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	UpdatedAt  time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
+	DeletedAt  gorm.DeletedAt `gorm:"index"`
+}
+
+type Messages struct {
+	MessageID  string         `gorm:"primaryKey;type:varchar(45)"`
+	UserID     string         `gorm:"foreignKey;type:varchar(45)"`
+	RoomRoomID string         `gorm:"foreignKey;type:varchar(45)"`
+	Message    string         `gorm:"type:varchar(225);not null"`
 	CreatedAt  time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	UpdatedAt  time.Time      `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	DeletedAt  gorm.DeletedAt `gorm:"index"`
